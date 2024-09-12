c13eddfecb326acd18690af9bf0d3c82f84a3c7c
Filipe Salviano <filipesalviano@proton.me>
2024-09-12 20:01:05 -0300

    Communication and redirect between projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatDialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'other-module';
+
+  ngOnInit(): void {
+    window.name = "othermodule";
+
+    window.addEventListener('message', this.handleMessage.bind(this), false);
+  }
+
+  handleMessage(event: MessageEvent) {
+    console.log(event);
+
+    if (event.origin === 'http://localhost:4200') {
+      const messageData = event.data;
+      console.log(messageData)
+    }
+  }
+
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
     <h2>Home Page</h2>
     <p>Welcome to the Home page!</p>
     <button (click)="goToAbout()">Go to About Page</button>
+    <button (click)="backDashboard()">Go to dashboard</button>
   `
 })
 export class HomeComponent {
@@ -15,5 +16,10 @@ export class HomeComponent {
 
   goToAbout() {
     this.router.navigate(['/about']);  // Navigate to the About page
+  }
+
+  backDashboard() {
+    let target = window.open("http://localhost:4200", "dashboard");
+    target?.postMessage("teste", "http://localhost:4200")
   }
 }
